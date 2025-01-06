56afe10280a5574bcedf73bd639a2a743637e320
d1syax <boyaka201@gmail.com>
2025-01-06 18:45:57 +0200

    refactor: replace 'for in' with 'for of'

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,13 +4,18 @@
 'use strict'
 
 const mergeTwoObj = (obj1, obj2) => {
-  const result = {}
-  for (const key in obj1) {
+  const result = {};
+  const firstKeys = Object.keys(obj1)
+  const secondKeys = Object.keys(obj2)
+  
+  for (const key of firstKeys) {
     result[key] = obj1[key];
   }
-  for (const key in obj2) {
+  
+  for (const key of secondKeys) {
     result[key] = obj2[key];
   }
+  
   return result;
 };
 
