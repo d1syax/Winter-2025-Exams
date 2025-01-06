964b7607777e1612d58876c74bfa4a723be96ee4
d1syax <boyaka201@gmail.com>
2025-01-06 12:05:41 +0200

    refactor: replace with lambda

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,7 +3,7 @@
 
 'use strict'
 
-const intersection = function intersection(object_1, object_2) {
+const intersection = (object_1, object_2) => {
   const first_keys = Object.keys(object_1);
   
   for (const attribute_name of first_keys) {
