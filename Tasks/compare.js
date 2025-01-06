dc66adbbbb47ee5e96284f19ba68d8b241565d64
d1syax <boyaka201@gmail.com>
2025-01-06 14:50:47 +0200

    refactor: make the function look cleaner, fix the algorithm

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -6,15 +6,15 @@
 const compare = (obj1, obj2) => {
   const firstKeys = Object.keys(obj1);
   const secondKeys = Object.keys(obj2);
-  if (firstKeys.join('-') !== secondKeys.join('-')) return false;
-  let areEqual = true;
-  for (const key of firstKeys) {
-    if (obj1[firstKeys] === obj2[secondKeys]) areEqual = areEqual && true;
-    else {
-      areEqual = areEqual && false;
-    }
+  if (firstKeys.join('-') !== secondKeys.join('-')) {
+    return false;
   }
-  return areEqual;
+  for (const key of firstKeys) {
+   if (obj1[key] !== obj2[key]) { 
+     return false
+   }
+  }
+  return true;
 };
 
 module.exports = compare;
