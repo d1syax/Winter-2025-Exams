2613e3ddaaa863ea3dd07b825ef8ef5c73841377
d1syax <boyaka201@gmail.com>
2025-01-06 18:58:53 +0200

    refactor: also in functional style

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,13 +3,9 @@
 
 'use strict'
 
-const countValue = (obj) => {
-  let sum = 0;
-  const values = Object.values(obj);
-  for (const value of values) {
-    if (typeof value === 'number') sum += value;
-  };
-  return sum;
-};
+const countValues = (obj) =>
+  Object.values(obj)
+    .filter((value) => typeof value === 'number')
+    .reduce((sum, num) => sum += num, 0);
 
 module.exports = countValue;
