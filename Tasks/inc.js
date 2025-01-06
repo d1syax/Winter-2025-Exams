47df9b57837c4e1f92ae10c160227b5eb92d45b5
d1syax <boyaka201@gmail.com>
2025-01-06 16:11:28 +0200

    refactor: simlify & improve logic

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -5,8 +5,8 @@
 
 const incNumbers = (object) => {
   for (const key in object) {
-    if ((typeof object[key]).charAt(0).toUpperCase() === 'N') {
-      object[key] = object[key] + 1;
+    if (typeof object[key] === 'number') {
+      object[key] += 1
     }
   }
   return object;
